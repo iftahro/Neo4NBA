397c80b1f95113d8e78cd61dec7942e1814fd15e
Iftah Roichman <ifttah@gmail.com>
2020-02-25 20:39:39 +0200

    Renamed copy_tree() Added graph files path const

--- source/config.py
+++ source/config.py
@@ -1,9 +1,12 @@
+import os
+
 # Neo4j driver
 NEO4J_USERNAME = "neo4j"
 NEO4J_PASSWORD = "Neo4j$"
 NEO4J_BOLT_ADDRESS = "bolt://localhost:7687"
 
-# General
+# Setup
+GRAPH_FILES_DIRECTORY_PATH = os.path.dirname(__file__).replace("source", "graph_files")
 NEO4J_IMPORT_DIRECTORY = r"D:\Programming\neo4j-community-4.0.0\import"
 SHOULD_COPY_FILES_TO_SERVER = False
 
