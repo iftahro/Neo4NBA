0c0cbc7244085546257f836a20a64e604362f037
Iftah Roichman <ifttah@gmail.com>
2020-03-15 22:28:49 +0200

    Fixed collection prop problem

--- source/graph_updaters/award/award_properties_graph_updater.py
+++ source/graph_updaters/award/award_properties_graph_updater.py
@@ -7,16 +7,12 @@ WITH toLower(m.short) AS award, w, a
 WITH replace(award," ","_") AS award_name, w, a
 ORDER BY w.year
 WITH COLLECT(w.year) AS v, award_name, a
-CALL apoc.create.setProperty([a], award_name, v) YIELD node
-RETURN node
-"""
-
-ADD_AWARD_LABEL = """
-MATCH (p:Player)-[:WON]->(a:Award)
-CALL apoc.create.addLabels([p], [a.short]) yield node RETURN NULL
+CALL apoc.do.when(SIZE(v) > 1,
+'CALL apoc.create.setProperty([a], award_name, v) YIELD node RETURN NULL',
+'CALL apoc.create.setProperty([a], award_name, v[0]) YIELD node RETURN NULL',
+{a:a, award_name:award_name, v:v}) YIELD value RETURN NULL
 """
 
 award_properties_graph_updater = GraphUpdater("award properties", [
-    ADD_AWARD_PROPERTY,
-    ADD_AWARD_LABEL
+    ADD_AWARD_PROPERTY
 ])
