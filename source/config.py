71af7f396d2f39221d2a01d3852cd16556e8f1f7
Iftah Roichman <ifttah@gmail.com>
2020-04-11 16:05:51 +0300

    Import dir fix

--- source/config.py
+++ source/config.py
@@ -4,7 +4,7 @@ NEO4J_PASSWORD = "neo4j"
 NEO4J_BOLT_ADDRESS = "bolt://localhost:7687"
 
 # Graph files
-NEO4J_IMPORT_DIRECTORY = r"C:\user\programming\neo4j-community-4.0.0\import"
+NEO4J_IMPORT_DIRECTORY = r"C:\user\neo4j-community-4.0.0\import"
 SHOULD_COPY_FILES_TO_SERVER = True
 
 # Startup reset
